8244a9a1aa892742a2a38bdf43f5842352f24584
Rafal Dudycz <rafal.dudycz@gmail.com>
2020-12-09 10:27:23 +0100

    Day 8, part 2

--- 2020/day8/src/main.rs
+++ 2020/day8/src/main.rs
@@ -3,27 +3,28 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::collections::HashSet;
 
-#[derive(Debug)]
+#[derive(Debug,Clone)]
 struct Instruction {
     op: String,
     arg: i32,
 }
 
-fn build_instruction(s: &String) -> Instruction {
-    let mut iter = s.split_whitespace();
-    match iter.next().unwrap() {
-        "nop" => Instruction {op: "nop".to_string(), arg: 0},
-        "jmp" => Instruction {op: "jmp".to_string(), arg: iter.next().unwrap().parse::<i32>().unwrap() },
-        "acc" => Instruction {op: "acc".to_string(), arg: iter.next().unwrap().parse::<i32>().unwrap() },
-        _ => panic!("Unsupported op code"),
+impl Instruction {
+    fn from_str(s: &str) -> Self {
+        let mut iter = s.split_whitespace();
+        Self {
+            op: iter.next().unwrap().into(),
+            arg: iter.next().unwrap().parse().unwrap()
+        }
     }
 }
 
-fn execute_part1(prog: &Vec<Instruction>) -> i32 {
+fn execute_program(prog: &Vec<Instruction>) -> (i32, bool) {
     let mut accumulator = 0;
     let mut executed = HashSet::new();
 
     let mut pc:i32 = 0;
+    let mut reached_end = false;
     while !executed.contains(&pc) {
         executed.insert(pc);
         let instr = &prog[pc as usize];
@@ -31,20 +32,44 @@ fn execute_part1(prog: &Vec<Instruction>) -> i32 {
             "nop" => pc += 1,
             "acc" => { accumulator += instr.arg; pc += 1},
             "jmp" => pc += instr.arg,
-            _ => (),
+            "end" => {reached_end = true; break},
+            _ => unimplemented!(),
         }
     }
-    accumulator
+    (accumulator, reached_end)
+}
+
+fn fix_endless_loop(prog: &Vec<Instruction>) -> i32 {
+    let mut ret = 0;
+    let mut fixed_prog = Vec::new();
+    for (i,instr) in prog.iter().enumerate() {
+        match instr.op.as_str() {
+            "nop" => {fixed_prog = prog.clone(); fixed_prog[i].op = String::from("jmp")},
+            "jmp" => {fixed_prog = prog.clone(); fixed_prog[i].op = String::from("nop")},
+            _ => continue,
+        }
+
+        let (acc, finished) = execute_program(&fixed_prog);
+
+        if finished {
+            ret = acc;
+            break;
+        }
+    }
+    ret
 }
 
 fn main() {
     if let Ok(lines) = read_lines("input") {
         let mut program:Vec<Instruction> = Vec::new();
         for line in lines {
-            program.push(build_instruction(&line.unwrap()));
+            program.push(Instruction::from_str(&line.unwrap()));
         }
+        program.push(Instruction::from_str("end 0"));
 
-        println!("Part 1: accumulator contains {}", execute_part1(&program));
+        println!("Part 1: accumulator contains {}", execute_program(&program).0);
+        println!("Part 2: accumulator after fix contains {}", fix_endless_loop(&program));
+
     }
 }
 
